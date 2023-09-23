73c34809a4255567ba698ce6b2fa61cc55666c10
Jinkouu <jonathan0174@live.com>
2023-09-23 23:54:49 +1000

    red ghost display

---
+++ Assets/Scripts/GhostAnimator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostAnimation : MonoBehaviour
+{
+    public Animator animatorController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+            //animatorController.SetTrigger("Left");
+            //
+            //animatorController.SetTrigger("Down");
+            //
+            //animatorController.SetTrigger("Right");
+            //
+            //animatorController.SetTrigger("Up");
+            //
+            //animatorController.SetTrigger("DeathLeft");
+            //
+            //animatorController.SetTrigger("DeathRight");
+            //
+            //animatorController.SetTrigger("DeathUp");
+            //
+            //animatorController.SetTrigger("DeathDown");
+            //
+            //animatorController.SetTrigger("Scared");
+            //
+            //animatorController.SetTrigger("Recovery");
+    }
+}
