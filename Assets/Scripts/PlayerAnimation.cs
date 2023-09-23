cb1c9ebe41bebe231b9140126305d630064236f5
Jinkouu <jonathan0174@live.com>
2023-09-23 21:37:10 +1000

    params

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animatorController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            animatorController.SetTrigger("Left");
+        if (Input.GetKeyDown(KeyCode.S))
+            animatorController.SetTrigger("Down");
+        if (Input.GetKeyDown(KeyCode.D))
+            animatorController.SetTrigger("Right");
+        if (Input.GetKeyDown(KeyCode.W))
+            animatorController.SetTrigger("Up");
+    }
+}
