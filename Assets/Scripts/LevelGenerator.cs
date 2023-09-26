92f93faea5a6179b9a7b4c3ff1b630cde7d18b86
Jinkouu <jonathan0174@live.com>
2023-09-26 22:45:05 +1000

    base code

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,10 +24,23 @@ public class LevelGenerator : MonoBehaviour
                 {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
             };
 
+    [SerializeField] private GameObject sprite1;
+    [SerializeField] private GameObject sprite2;
+    [SerializeField] private GameObject sprite3;
+    [SerializeField] private GameObject sprite4;
+    [SerializeField] private GameObject sprite5;
+    [SerializeField] private GameObject sprite6;
+    [SerializeField] private GameObject sprite7;
+
+    //private int startX = -10;
+    //private int startY = 13;
+    private int startX = 0;
+    private int startY = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        generate(levelMap);
     }
 
     // Update is called once per frame
@@ -41,9 +55,30 @@ public class LevelGenerator : MonoBehaviour
         {
             for (int j = 0; j < levelMap.GetLength(1); j++)
             {
-                switch(levelMap[i,j])
+                switch (levelMap[i,j])
                 {
                     case 0: //do nothing
+                        break;
+                    case 1:
+                        Instantiate(sprite1, new Vector3(startX + j, startY - i, 0), Quaternion.identity);
+                        break;
+                    case 2:
+                        Instantiate(sprite2, new Vector3(startX + j, startY - i, 0), Quaternion.identity);
+                        break;
+                    case 3:
+                        Instantiate(sprite3, new Vector3(startX + j, startY - i, 0), Quaternion.identity);
+                        break;
+                    case 4:
+                        Instantiate(sprite4, new Vector3(startX + j, startY - i, 0), Quaternion.identity);
+                        break;
+                    case 5:
+                        Instantiate(sprite5, new Vector3(startX + j, startY - i, 0), Quaternion.identity);
+                        break;
+                    case 6:
+                        Instantiate(sprite6, new Vector3(startX + j, startY - i, 0), Quaternion.identity);
+                        break;
+                    case 7:
+                        Instantiate(sprite7, new Vector3(startX + j, startY - i, 0), Quaternion.identity);
                         break;
 
                 }
