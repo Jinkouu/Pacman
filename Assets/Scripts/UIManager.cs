34b3b4a41377ecda8d333e0db0444b774ac6245c
Jinkouu <jonathan0174@live.com>
2023-10-30 21:21:41 +1100

    start screen player prefs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,13 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            try
+            {
+                GameObject timerObject = GameObject.FindGameObjectWithTag("Timer");
+                Text timeText = timerObject.GetComponent<Text>();
+                timeText.text = PlayerPrefs.GetString("BestTime", "00:00:00");
+
+                GameObject scoreObject = GameObject.FindGameObjectWithTag("Score");
+                Text scoreText = scoreObject.GetComponent<Text>();
+                scoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+            }
+            catch
+            {
+
+            }
+        }
     }
 
     // Update is called once per frame
